28367c4ac556c06348c2f2ada89d62bdcb8d4a53
lyf1995notebook <506833107@qq.com>
2018-05-07 10:17:49 +0800

    修改数据库表

--- doc/wechat-mall.sql
+++ doc/wechat-mall.sql
@@ -10,75 +10,75 @@ Target Server Type    : MYSQL
 Target Server Version : 50714
 File Encoding         : 65001
 
-Date: 2018-05-05 21:43:03
+Date: 2018-05-07 10:17:24
 */
 
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
--- Table structure for address
+-- Table structure for t_address
 -- ----------------------------
-DROP TABLE IF EXISTS `address`;
-CREATE TABLE `address` (
+DROP TABLE IF EXISTS `t_address`;
+CREATE TABLE `t_address` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `contacts` varchar(255) DEFAULT NULL,
-  `phone` varchar(255) DEFAULT '',
-  `province` varchar(255) DEFAULT NULL,
-  `province_city` varchar(255) DEFAULT NULL,
-  `area` varchar(255) DEFAULT NULL,
-  `detail_address` varchar(255) DEFAULT NULL,
+  `contacts` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `phone` varchar(255) CHARACTER SET utf8 DEFAULT '',
+  `province` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `province_city` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `area` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `detail_address` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
   `user_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `userId` (`user_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- ----------------------------
--- Records of address
+-- Records of t_address
 -- ----------------------------
 
 -- ----------------------------
--- Table structure for goods
+-- Table structure for t_goods
 -- ----------------------------
-DROP TABLE IF EXISTS `goods`;
-CREATE TABLE `goods` (
+DROP TABLE IF EXISTS `t_goods`;
+CREATE TABLE `t_goods` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT '',
+  `name` varchar(255) CHARACTER SET utf8 DEFAULT '',
   `vip_price` float(10,0) DEFAULT NULL,
   `price` float(10,0) DEFAULT NULL,
-  `subtitle` varchar(255) DEFAULT '',
+  `subtitle` varchar(255) CHARACTER SET utf8 DEFAULT '',
   `stock` float(10,0) DEFAULT NULL,
-  `main_image` varchar(255) DEFAULT NULL,
-  `carousel_image` varchar(255) DEFAULT '',
-  `detail_image` varchar(255) DEFAULT '',
+  `main_image` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `carousel_image` varchar(255) CHARACTER SET utf8 DEFAULT '',
+  `detail_image` varchar(255) CHARACTER SET utf8 DEFAULT '',
   `type_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `typeId` (`type_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- ----------------------------
--- Records of goods
+-- Records of t_goods
 -- ----------------------------
 
 -- ----------------------------
--- Table structure for goodstype
+-- Table structure for t_goodstype
 -- ----------------------------
-DROP TABLE IF EXISTS `goodstype`;
-CREATE TABLE `goodstype` (
+DROP TABLE IF EXISTS `t_goodstype`;
+CREATE TABLE `t_goodstype` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `type_name` varchar(255) DEFAULT '',
-  `type_describe` varchar(255) DEFAULT NULL,
+  `type_name` varchar(255) CHARACTER SET utf8 DEFAULT '',
+  `type_describe` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- ----------------------------
--- Records of goodstype
+-- Records of t_goodstype
 -- ----------------------------
 
 -- ----------------------------
--- Table structure for goods_order
+-- Table structure for t_goods_order
 -- ----------------------------
-DROP TABLE IF EXISTS `goods_order`;
-CREATE TABLE `goods_order` (
+DROP TABLE IF EXISTS `t_goods_order`;
+CREATE TABLE `t_goods_order` (
   `id` int(11) NOT NULL,
   `goods_id` int(11) NOT NULL,
   `order_id` int(11) NOT NULL,
@@ -88,21 +88,21 @@ CREATE TABLE `goods_order` (
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- ----------------------------
--- Records of goods_order
+-- Records of t_goods_order
 -- ----------------------------
 
 -- ----------------------------
--- Table structure for order
+-- Table structure for t_order
 -- ----------------------------
-DROP TABLE IF EXISTS `order`;
-CREATE TABLE `order` (
+DROP TABLE IF EXISTS `t_order`;
+CREATE TABLE `t_order` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `number` varchar(255) DEFAULT '',
-  `status` varchar(255) DEFAULT '',
+  `number` varchar(255) CHARACTER SET utf8 DEFAULT '',
+  `status` varchar(255) CHARACTER SET utf8 DEFAULT '',
   `user_id` int(11) NOT NULL,
   `address_id` int(11) NOT NULL,
   `total_amount` float(255,0) DEFAULT NULL,
-  `remarks` varchar(255) DEFAULT '',
+  `remarks` varchar(255) CHARACTER SET utf8 DEFAULT '',
   `order_time` datetime DEFAULT NULL,
   `type` int(255) NOT NULL,
   PRIMARY KEY (`id`),
@@ -111,14 +111,14 @@ CREATE TABLE `order` (
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- ----------------------------
--- Records of order
+-- Records of t_order
 -- ----------------------------
 
 -- ----------------------------
--- Table structure for share
+-- Table structure for t_share
 -- ----------------------------
-DROP TABLE IF EXISTS `share`;
-CREATE TABLE `share` (
+DROP TABLE IF EXISTS `t_share`;
+CREATE TABLE `t_share` (
   `id` int(11) NOT NULL,
   `share_user_id` int(11) NOT NULL,
   `goods_id` int(11) NOT NULL,
@@ -130,31 +130,31 @@ CREATE TABLE `share` (
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- ----------------------------
--- Records of share
+-- Records of t_share
 -- ----------------------------
 
 -- ----------------------------
--- Table structure for share_user
+-- Table structure for t_share_user
 -- ----------------------------
-DROP TABLE IF EXISTS `share_user`;
-CREATE TABLE `share_user` (
+DROP TABLE IF EXISTS `t_share_user`;
+CREATE TABLE `t_share_user` (
   `id` int(11) NOT NULL,
   `share_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
-  `status` varchar(255) DEFAULT NULL,
+  `status` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
   `amounts` float(255,0) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- ----------------------------
--- Records of share_user
+-- Records of t_share_user
 -- ----------------------------
 
 -- ----------------------------
--- Table structure for shoppingcar
+-- Table structure for t_shoppingcar
 -- ----------------------------
-DROP TABLE IF EXISTS `shoppingcar`;
-CREATE TABLE `shoppingcar` (
+DROP TABLE IF EXISTS `t_shoppingcar`;
+CREATE TABLE `t_shoppingcar` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
   `goods_id` int(11) NOT NULL,
@@ -165,23 +165,31 @@ CREATE TABLE `shoppingcar` (
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- ----------------------------
--- Records of shoppingcar
+-- Records of t_shoppingcar
 -- ----------------------------
 
 -- ----------------------------
--- Table structure for user
+-- Table structure for t_user
 -- ----------------------------
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user` (
+DROP TABLE IF EXISTS `t_user`;
+CREATE TABLE `t_user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `phone` varchar(255) DEFAULT '',
-  `password` varchar(255) DEFAULT '',
-  `name` varchar(255) DEFAULT '',
+  `phone` varchar(255) CHARACTER SET utf8 DEFAULT '',
+  `password` varchar(255) CHARACTER SET utf8 DEFAULT '',
+  `name` varchar(255) CHARACTER SET utf8 DEFAULT '',
   `regist_time` datetime DEFAULT NULL,
   `recent_login_time` datetime DEFAULT NULL,
+  `is_delete` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `type` int(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
--- Records of user
+-- Records of t_user
 -- ----------------------------
+INSERT INTO `t_user` VALUES ('1', 'admin', 'admin', '系统管理员', '2018-05-06 00:00:00', '2018-05-06 22:52:43', '0', '0');
+INSERT INTO `t_user` VALUES ('2', '17826804112', '123', '张三', '2018-05-06 21:32:15', null, '0', '1');
+INSERT INTO `t_user` VALUES ('3', '12312312312', '123', '李四', '2018-05-06 21:16:14', null, '0', '1');
+INSERT INTO `t_user` VALUES ('4', '17826804000', '123', '王五', '2018-05-06 21:18:06', null, '0', '1');
+INSERT INTO `t_user` VALUES ('5', '17474847811', '123', '赵六', '2018-05-06 21:46:25', null, '0', '1');
+INSERT INTO `t_user` VALUES ('6', '12345678901', '123', '陈七', '2018-05-06 23:04:58', null, '0', '1');
